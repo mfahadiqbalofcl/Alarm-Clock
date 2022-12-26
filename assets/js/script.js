79c91874157077c8f3c27e0f651cdf997df96a4a
M Fahad Iqbal <74042506+mfahadiqbalofcl@users.noreply.github.com>
2022-12-26 17:47:08 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ selectMenu = document.querySelectorAll("select"),
 setAlarmBtn = document.querySelector("button");
 
 let alarmTime, isAlarmSet,
-ringtone = new Audio("./files/ringtone.mp3");
+ringtone = new Audio("./assets/files/ringtone.mp3");
 
 for (let i = 12; i > 0; i--) {
     i = i < 10 ? `0${i}` : i;
